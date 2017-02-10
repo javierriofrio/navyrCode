bdc297c3d9e277705573321e38b496073d46445d
Javier Riofrio <javierriofrio@gmail.com>
2017-02-10 17:08:16 -0500

    mapa y camara

--- src/pages/micuenta/micuenta.ts
+++ src/pages/micuenta/micuenta.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'page-micuenta',
   templateUrl: 'micuenta.html'
 })
 export class MiCuenta {
+  cameraData: string;
+  photoTaken: boolean;
+  cameraUrl: string;
+  photoSelected: boolean;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private DomSanitizer: DomSanitizer) {
+    this.photoTaken = false;
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+  tomarFoto() {
+    var options = {
+      sourceType: Camera.PictureSourceType.CAMERA,
+      destinationType: Camera.DestinationType.DATA_URL
+    };
+    Camera.getPicture(options).then((imageData) => {
+      this.cameraData = 'data:image/jpeg;base64,' + imageData;
+      this.photoTaken = true;
+      this.photoSelected = false;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
