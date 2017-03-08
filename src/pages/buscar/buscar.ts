5560dcb467c0a6eb80b263d151a529cf50adef1a
javierriofrio <javierriofrio@gmail.com>
2017-03-07 22:50:07 -0500

    ultimos cambios

--- src/pages/buscar/buscar.ts
+++ src/pages/buscar/buscar.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Navyr } from '../navyr/navyr';
-
+import { Establecimiento } from '../establecimiento/establecimiento';
 /*
   Generated class for the Buscar page.
 
@@ -13,6 +13,9 @@ import { Navyr } from '../navyr/navyr';
   templateUrl: 'buscar.html'
 })
 export class Buscar {
+  searchQuery: string = '';
+  items: string[];
+  establecimiento = Establecimiento
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
@@ -24,4 +27,30 @@ export class Buscar {
 	  this.navCtrl.setRoot(Navyr);
   }
 
+
+  initializeItems() {
+    this.items = [
+      'Pizza Hut',
+      'Chefarina',
+      'KFC',
+      'El Rincon',
+      'Tabule'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
+
